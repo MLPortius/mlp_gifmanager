b1f125385738754d73df8e8211df634f1da8e963
mlportius <mlportius@gmail.com>
2024-02-12 08:00:49 -0300

    VIDEO BUILDER AND GIF MODIFIER NOTEBOOKS

---
+++ video_builder.py
@@ -0,0 +1,12 @@
+#%% IMPORTAR LIBRERIAS
+import os
+import time
+import tqdm
+import argparse
+
+
+#%% CONFIGURAR ENTORNO
+infolder = 'video_builder/input/'
+outfolder = 'video_builder/output/'
+
+#%% 
